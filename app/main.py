b86914c38eb6147e9544aabcbf6e35f02e9abc94
juwon <jwontiger@gmail.com>
2025-05-19 13:56:32 +0900

    fix : eureka
    eureka custom -> eureka open source lib

--- app/main.py
+++ app/main.py
@@ -1,24 +1,37 @@
 from fastapi import FastAPI
-from app.api.v1 import discussion  # discussion.py 안에 있는 router 불러오기
+from app.api.v1 import discussion
 from dotenv import load_dotenv
 import os
-from app.eureka_client.client import register_with_eureka  # ✅ Eureka 등록 함수
+
+from py_eureka_client import eureka_client
+from app.config.eureka_config import APP_NAME, PORT, EUREKA_URL
+
+import asyncio
 
 # 환경변수 로딩
 load_dotenv()
 
-# 디버깅용 출력 (나중에 제거 가능)
 print("✅ GROQ_API_KEY =", os.getenv("GROQ_API_KEY"))
 
 app = FastAPI()
 
-# discussion 라우터 등록
+# 라우터 등록
 app.include_router(discussion.router, prefix="/api/v1", tags=["discussion"])
 
 @app.on_event("startup")
 async def startup_event():
-    # 동기 함수는 await 없이 호출
-    register_with_eureka()
+    # ✅ 비동기 Eureka 등록
+    await eureka_client.init_async(
+        eureka_server=EUREKA_URL,
+        app_name=APP_NAME,
+        instance_port=PORT,
+        instance_ip="127.0.0.1",
+        health_check_url=f"http://localhost:{PORT}/",
+        home_page_url=f"http://localhost:{PORT}/",
+        status_page_url=f"http://localhost:{PORT}/status",
+        renewal_interval_in_secs=30,
+        duration_in_secs=90,
+    )
 
 @app.get("/")
 def hello():
