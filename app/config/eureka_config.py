d50a1967f6d8d162526bc92426bc43c3fd09fed8
juwon <jwontiger@gmail.com>
2025-06-02 10:32:39 +0900

    Delete eureka_config.py

--- app/config/eureka_config.py
+++
@@ -1,7 +0,0 @@
-import os
-import uuid
-
-APP_NAME = os.getenv("APP_NAME", "discussion-room")
-PORT = int(os.getenv("PORT", 8000))
-INSTANCE_ID = f"{APP_NAME}:{uuid.uuid4()}"
-EUREKA_URL = os.getenv("EUREKA_URL", "http://localhost:8761/eureka")
